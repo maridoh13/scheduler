3076b8a8690637852f38678244a97f88ecbf4483
Mariana Dominguez <maridoh@gmail.com>
2020-03-17 18:59:19 -0700

    added all Appoitment components

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "../src/components/InterviewerList";
 import Appointment from "../src/components/Appointment";
 import Header from "../src/components/Appointment/Header";
 import Empty from "../src/components/Appointment/Empty";
+import Show from "../src/components/Appointment/Show";
+import Confirm from "../src/components/Appointment/Confirm";
+import Status from "../src/components/Appointment/Status";
+import Error from "../src/components/Appointment/Error";
 
 
 storiesOf("Button", module)
@@ -139,3 +143,16 @@ storiesOf("Appointment", module)
   .add("Appointment with time", () => <Appointment time="12pm"/>)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show 
+      student="Lydia Miller-Jones"  
+      interviewer={interviewer.name}
+      onEdit={action("onEdit")}
+      onDelete={action('onDelete me!')}
+      />)
+  .add("Confirm", () => <Confirm 
+      message="Delete the appointment?" 
+      onConfirm={action("onConfirm dis!")}
+      onCancel={action("onCancel yas!")}
+      />)
+  .add("Status", () => <Status message="Deleting dis"/>)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose dis")}   />)
