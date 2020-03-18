1af2d7e36f444f3bb5dd5caa77dcc0fc25980184
Mariana Dominguez <maridoh@gmail.com>
2020-03-17 23:13:45 -0700

    added apointments but did not load on host

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-import Button from "./Button";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -22,10 +22,57 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview : {
+      student: "Archie Cohen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview : {
+      student: "Maria Boucher",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png"
+      }
+    }
+  }
+
+];
+
 
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const schedule = appointments.map(a => 
+    <Appointment key={a.id} {...a} />)
 
   return (
     <main className="layout">
@@ -53,7 +100,8 @@ export default function Application(props) {
 
 
       <section className="schedule">
-              
+        { schedule }
+        <Appointment key="last" time="5pm" />
       </section>
 
 
