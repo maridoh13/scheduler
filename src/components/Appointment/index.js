762009d08e4c6e2d9265e17464ddf611692dfff0
Mariana Dominguez <maridoh@gmail.com>
2020-03-17 22:46:39 -0700

    components in

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./style.scss";
+import Header from "../Appointment/Header";
+import Show from "../Appointment/Show";
+import Empty from "../Appointment/Empty";
 
 
-export default function Appointment() {
+
+export default function Appointment(props) {
+  let display = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>
+  
   return (
     <article className="appointment">
-
+      <Header time={props.time}/>
+      { display }
     </article>
   );
 }
